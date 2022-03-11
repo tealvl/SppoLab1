5e3780bc17722f5931ae1ed0960991f9c7c15e61
Егор <89208912+Kirilloker@users.noreply.github.com>
2022-03-11 19:30:42 +0300

    Creare Class
    Добавил объявление классов

---
+++ Script/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SppoLab1
+{
+    class App
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, World!");
+        }
+    }
+}
