b01b2f8e2a779da319ab50c9861af12de373f914
Егор <89208912+Kirilloker@users.noreply.github.com>
2022-03-21 19:47:57 +0300

    Work

--- Script/Work/Work.cs
+++ Script/Work/Work.cs
@@ -8,6 +8,35 @@ namespace SppoLab1
 {
     class Work
     {
+        public readonly string name;
+        public readonly string workDiscription;
+        private List<Task> listTask;
 
+        public Work(string _name, string _workDiscription) 
+        {
+            name = _name;
+            workDiscription = _workDiscription;
+        }
+
+        public string GetFullInfo() 
+        {
+            string str = workDiscription +
+                "\n" + "'" + name + "'" + "\n" +
+                "Задания:" + "\n";
+                
+
+            for (int i = 0; i < listTask.Count; i++)
+            {
+                str += "\t" + listTask[i].text + "\n";
+            }
+
+
+            return str;
+        }
+
+        public void AddTask(Task _task) 
+        {
+            listTask.Add(_task);
+        }
     }
 }
