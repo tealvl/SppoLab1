21f313c56ce2e5d92b72516335fdc5bf23c58392
tealvl <tereshin-alexey01@yandex.ru>
2022-03-22 00:24:36 +0300

    builder

---
+++ Script/WorkCreator/IWorkBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SppoLab1
+{
+    interface IWorkBuilder
+    {
+        public void Reset();
+
+        public void AddTask(string _taskText);
+
+        public void SetName(string _name);
+
+        public void SetWorkDiscription(string _workDiscription);
+    }
+}
