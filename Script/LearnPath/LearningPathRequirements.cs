09d4547f6c165be2c65e01dd0582535ae1b37a74
tealvl <tereshin-alexey01@yandex.ru>
2022-03-24 04:21:17 +0300

    LPR in Admin

--- Script/LearnPath/LearningPathRequirements.cs
+++ Script/LearnPath/LearningPathRequirements.cs
@@ -6,49 +6,4 @@ using System.Threading.Tasks;
 
 namespace SppoLab1
 {
-    public class LearningPathRequirements
-    {
-        private List<ReqirementCourse> reqiremetCourses;
-        private uint minNumOptionalCourses;
-        
-        static private LearningPathRequirements singletoneLearningPathRequirements;
-
-        private LearningPathRequirements() 
-        {
-            singletoneLearningPathRequirements = this;
-            reqiremetCourses = new List<ReqirementCourse>();
-            minNumOptionalCourses = 0;
-        }
-
-        public void ChangeMinNumOptionalCourses(uint _minNumOptionalCourses)
-        {
-            minNumOptionalCourses = _minNumOptionalCourses;
-            UpdateLearnPathesRequirements();
-        }
-
-        public void SetReqiremetCourses(List<ReqirementCourse> _reqiremetCourses)
-        {
-            reqiremetCourses = _reqiremetCourses;
-            UpdateLearnPathesRequirements();
-        }
-
-        public void AddRequirementCourse(ReqirementCourse newReqirementCourse)
-        {
-            reqiremetCourses.Add(newReqirementCourse);
-            UpdateLearnPathesRequirements();
-        }
-
-        static public LearningPathRequirements Initialize()
-        {
-            if (singletoneLearningPathRequirements == null)
-                return new LearningPathRequirements();
-
-            return singletoneLearningPathRequirements;
-        }
-
-        public void UpdateLearnPathesRequirements()
-        {
-            LearnPath.UpdateRequirements(minNumOptionalCourses, reqiremetCourses);
-        }
-    }
 }
--- Script/Users/Administrator.cs
+++ Script/Users/Administrator.cs
@@ -9,11 +9,54 @@ namespace SppoLab1
 {
     class Administrator
     {
-        public void SignIn() 
+        public void SignIn()
         {
             UI.Print("Так так вы хотите войти как администратор, ну уж нет, пока!");
 
             App.SignIn();
+            LearningPathRequirements lpr = LearningPathRequirements.Initialize();
+        }
+
+
+
+        private class LearningPathRequirements
+        {
+            private List<ReqirementCourse> reqiremetCourses;
+            private uint minNumOptionalCourses;
+
+            static private LearningPathRequirements singletoneLearningPathRequirements;
+
+            public void ChangeMinNumOptionalCourses(uint _minNumOptionalCourses)
+            {
+                minNumOptionalCourses = _minNumOptionalCourses;
+                UpdateLearnPathesRequirements();
+            }
+
+            public void SetReqiremetCourses(List<ReqirementCourse> _reqiremetCourses)
+            {
+                reqiremetCourses = _reqiremetCourses;
+                UpdateLearnPathesRequirements();
+            }
+
+            public void AddRequirementCourse(ReqirementCourse newReqirementCourse)
+            {
+                reqiremetCourses.Add(newReqirementCourse);
+                UpdateLearnPathesRequirements();
+            }
+
+
+            static public LearningPathRequirements Initialize()
+            {
+                if (singletoneLearningPathRequirements == null)
+                    return new LearningPathRequirements();
+
+                return singletoneLearningPathRequirements;
+            }
+
+            public void UpdateLearnPathesRequirements()
+            {
+                LearnPath.UpdateRequirements(minNumOptionalCourses, reqiremetCourses);
+            }
         }
     }
 }
