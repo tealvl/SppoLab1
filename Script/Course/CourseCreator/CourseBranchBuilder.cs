082f3e26c99655f82f1691a645fd84d3bfd9b044
tealvl <tereshin-alexey01@yandex.ru>
2022-03-28 21:40:49 +0300

    CourseBuilder

---
+++ Script/Course/CourseCreator/CourseBranchBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SppoLab1
+{
+    class CourseBranchBuilder: AbstractCourseBuilder
+    {
+        CourseBranchBuilder()
+        {
+            course = new CourseBranch();
+        }
+
+        public void Reset()
+        {
+            course = new CourseBranch();
+        }
+
+        public void AddCourse(Course _someCourse)
+        {
+            course.AddCource(_someCourse);
+        }
+    }
+}
---
+++ Script/Course/CourseCreator/CourseBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SppoLab1.Script.Course.CourseCreator
+{
+    class CourseBuilder: AbstractCourseBuilder
+    {
+        CourseBuilder()
+        {
+            course = new Course();
+        }
+
+        public void SetName(string _name)
+        {
+            course.Name = _name;
+        }
+
+        public void SetWorkDiscription(string _workDiscription)
+        {
+            course.CourseDiscription = _workDiscription;
+        }
+
+        public void AddWork(Work _someWork)
+        {
+            course.AddWork(_someWork);
+        }
+
+        public void Reset()
+        {
+            course = new Course();
+        }
+    }
+}
